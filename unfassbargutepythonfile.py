de1a8615e071fcdaeb37f249e19b3cd5fea01c13
Friedfisch19 <friedrichreichardt@t-online.de>
2025-06-26 15:16:26 +0200

    neue Versionen

--- unfassbargutepythonfile.py
+++ unfassbargutepythonfile.py
@@ -1,7 +1,6 @@
 import time
-
-
-
+import random
+"""
 zeit = time.time()
 wartezeit = time.sleep("5")
 name="ich"
@@ -9,8 +8,15 @@ name="ich"
 # aktuelle Uhrzeit anzeigen
 print("Die aktuelle Uhrzeit ist:", time.strftime("%H:%M", time.localtime(zeit)), " Uhr")
 
-"""angabe wie viel Zeit gewartet wurde bis dieser Print ausgeführt wird"""
+#angabe wie viel Zeit gewartet wurde bis dieser Print ausgeführt wird#
 print(f"Wow es sind {wartezeit} Sekunden vergangen")
 
-    print(f"Hallo ich bin {name}")
+      print(f"Hallo ich bin {name}")
+"""
+def random_numbers():
+    seed = random.seed()
+    print(seed)
 
+
+
+random_numbers()
