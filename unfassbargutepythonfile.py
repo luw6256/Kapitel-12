863cf2e661e48318951aa38c851c21c6940c1e95
luca whitcombe <luw6256@thi.de>
2025-06-26 17:01:36 +0200

    lla

--- unfassbargutepythonfile.py
+++ unfassbargutepythonfile.py
@@ -6,23 +6,25 @@ import pandas
 """Variablen Deklaration"""
 zeit = time.time()
 wartezeit = time.sleep("5")
-name="ich"
+name = "ich"
 
-"""
+
 # aktuelle Uhrzeit anzeigen
 print("Die aktuelle Uhrzeit ist:", time.strftime("%H:%M", time.localtime(zeit)), " Uhr")
 
-#angabe wie viel Zeit gewartet wurde bis dieser Print ausgeführt wird#
+# angabe wie viel Zeit gewartet wurde bis dieser Print ausgeführt wird#
 print(f"Wow es sind {wartezeit} Sekunden vergangen")
 
-      print(f"Hallo ich bin {name}")
-"""
+print(f"Hallo ich bin {name}")
+
 
 def random_numbers():
     seed = random.seed()
 
     for i in range(10, seed.randint(10, 100)):
         yield i
+
+
 """
 def addnumbers():
     for j in random_numbers():
@@ -30,133 +32,22 @@ def addnumbers():
 """
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#hatte gehofft das reicht um diese Funktion zu verstecken
+# hatte gehofft das reicht um diese Funktion zu verstecken
 def errormessages():
     try:
         random_numbers()
         print("Ooh Baby, i feel like, This print sounds better than an Error ")
     except Exception as inst:
-        print("Ooops i did it again. i played with your Code, and have a problem again.")
+        print(
+            "Ooops i did it again. i played with your Code, and have a problem again."
+        )
         print(type(inst))
         print(inst.args)
         print(inst)
-        print("Ooops i did it again. i played with your Code, and have a problem again.")
+        print(
+            "Ooops i did it again. i played with your Code, and have a problem again."
+        )
+
 
 random_numbers()
 errormessages()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
