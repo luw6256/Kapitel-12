496189c59ff24862937ee2ce3fc9383adfa90c75
Friedfisch19 <friedrichreichardt@t-online.de>
2025-06-26 16:11:10 +0200

    Aufgaben v1.0

--- unfassbargutepythonfile.py
+++ unfassbargutepythonfile.py
@@ -1,10 +1,12 @@
 import time
 import random
-"""
+
+"""Variablen Deklaration"""
 zeit = time.time()
 wartezeit = time.sleep("5")
 name="ich"
 
+"""
 # aktuelle Uhrzeit anzeigen
 print("Die aktuelle Uhrzeit ist:", time.strftime("%H:%M", time.localtime(zeit)), " Uhr")
 
@@ -13,10 +15,140 @@ print(f"Wow es sind {wartezeit} Sekunden vergangen")
 
       print(f"Hallo ich bin {name}")
 """
+
 def random_numbers():
     seed = random.seed()
     print(seed)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#hatte gehofft das reicht um diese Funktion zu verstecken
+def errormessages():
+    try:
+        random_numbers()
+        print("Ooh Baby, i feel like, The code sounds better without an Error ")
+    except Exception as inst:
+        print("Ooops i did it again. i played with your Code, and have a problem again.")
+        print(type(inst))
+        print(inst.args)
+        print(inst)
+        print("Ooops i did it again. i played with your Code, and have a problem again.")
+
 random_numbers()
+errormessages()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
